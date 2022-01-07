85501cb48a83330e3f7c73094a8c3209a6203036
Quentin Guerrier <qguerrier@myc.doctor>
2022-01-07 12:23:17 +0100

    scss build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 const { src, dest } = require("gulp");
 
+// scss
+const dartSass = require('dart-sass');
+const gulpSass = require('gulp-sass');
+const sass = gulpSass( dartSass );
+
 const path = './src/';
 const dirBuildName = "dist"
 
+// Copy element in the directory "dirBuilName"
 function copy(cb) {
   src([path + '**/*.js', path + '**/*.html'])
     .pipe(dest(dirBuildName));
   cb();
 }
 
+// Generate CSS form SCSS
+function generateCSS(cb) {
+  src(path + 'scss/init.css')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest(dirBuildName))
+  cb();
+}
+
+
+
 exports.copy = copy;
+exports.css = generateCSS;
