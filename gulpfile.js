2822dfd88230178925e5a01e7f340a7498dbacc3
Quentin Guerrier <quentinguerrier@MacBook-Pro-de-Quentin.home>
2022-01-09 15:59:36 +0100

    add the templating

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,50 @@
+// Gulp
 const { src, dest } = require("gulp");
 
 // scss
-const dartSass = require('dart-sass');
-const gulpSass = require('gulp-sass');
-const sass = gulpSass( dartSass );
+const sass = require('gulp-sass')(require('sass'));
 
+// html 
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+
+// files
+const fs = require('fs');
+
+// Global variables
 const path = './src/';
 const dirBuildName = "dist"
 
-// Copy element in the directory "dirBuilName"
-function copy(cb) {
-  src([path + '**/*.js', path + '**/*.html'])
-    .pipe(dest(dirBuildName));
-  cb();
-}
-
 // Generate CSS form SCSS
 function generateCSS(cb) {
-  src(path + 'scss/init.css')
+  src(path + 'scss/init.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(rename({
+      basename: "app"
+    }))
     .pipe(dest(dirBuildName))
   cb();
 }
 
+// Generate HTML 
+function generateHTML(cb){
+  fs.readdir(path + 'views/', (err, files) => {
+    files.forEach(file => {
+      src(path + 'views/' + file)
+      .pipe(ejs({
+        global: {
+          cssLink: "app.css",
+        },
+        data: require(path + "functions/" + file.substr(0, file.lastIndexOf(".")) + ".js")
+      }))
+      .pipe(rename({
+        extname: ".html"
+      }))
+      .pipe(dest("dist"));
+    });
+  });
+  cb();
+}
 
-
-exports.copy = copy;
 exports.css = generateCSS;
+exports.html = generateHTML;
